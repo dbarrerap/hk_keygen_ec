603402d1061abebe75c1ddadbf760f954f63c58a
David E. Barrera <dbarrerap@live.com>
2024-09-15 06:50:39 -0500

    Nuevo juego: Hide Ball

--- keygen.js
+++ keygen.js
@@ -90,6 +90,7 @@ document.addEventListener('DOMContentLoaded', () => {
         FLUFFCRUSADE: 'Fluff Crusade',
         STONEAGE: 'Stone Age',
         BOUNCE: 'Bouncemasters',
+        HIDEBALL: 'Hide Ball',
     })
 
     for (const [value, label] of Object.entries(apps)) {
@@ -117,6 +118,7 @@ document.addEventListener('DOMContentLoaded', () => {
         FLUFFCRUSADE: Object.freeze({ token: '112887b0-a8af-4eb2-ac63-d82df78283d9', promo: '112887b0-a8af-4eb2-ac63-d82df78283d9', attempts: 10, delay: 4e4 }),
         STONEAGE: Object.freeze({ token: '04ebd6de-69b7-43d1-9c4b-04a6ca3305af', promo: '04ebd6de-69b7-43d1-9c4b-04a6ca3305af', attempts: 20, delay: 2e4 }),
         BOUNCE: Object.freeze({ token: 'bc72d3b9-8e91-4884-9c33-f72482f0db37', promo: 'bc72d3b9-8e91-4884-9c33-f72482f0db37', attempts: 10, delay: 2e4 }),
+        HIDEBALL: Object.freeze({ token: '4bf4966c-4d22-439b-8ff2-dc5ebca1a600', promo: '4bf4966c-4d22-439b-8ff2-dc5ebca1a600', attempts: 20, delay: 3e4 }),
     })
     const generateId = (selectedGame) => {
         const fecha = Date.now();
@@ -268,4 +270,5 @@ document.addEventListener('DOMContentLoaded', () => {
  * 1.2.11 Nuevo juego, Tile Trio. Ammend: Fluff Crusade
  * 1.2.12 Nuevo Juego: Stone Age
  * 1.2.13 Nuevo juego: Bouncemasters
+ * 1.2.14 Nuevo juego: Hide Ball
  */
