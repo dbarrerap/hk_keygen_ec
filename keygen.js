b1d34d56d5504c273997460408a078fbe69d48f9
David E. Barrera <dbarrerap@live.com>
2024-10-21 19:55:09 -0500

    Nuevo arreglo para hacer uso de una sola estructura de datos

--- keygen.js
+++ keygen.js
@@ -7,9 +7,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const txtCode = document.querySelector('#txt-code')
     const cmbSelectedGame = document.querySelector('#cmb-game')
     const ul = document.querySelector('#messages')
-    let pBarPercentage = 0;
     const pBarText = document.querySelector('#pBarText')
     const btnRestore = document.querySelector('#btn-addon-restore')
+    let pBarPercentage = 0;
 
     // Events
     btnCopy.addEventListener('click', () => {
@@ -73,67 +73,72 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 
     // APPS Enum
-    const apps = Object.freeze({
-        TRAIN: 'Train Miner',
-        FCTRY: 'Factory World',
-        COUNT: 'Count Master',
-        SNAKE: 'Snake Run',
-        MERGE: 'Merge Away',
-        COOKST: 'Cooking Stories',
-        CUBE: 'Chain Cube 2048',
-        INFCT: 'Infected Frontier',
-        BOUNCE: 'Bouncemasters',
-        POLYSPHERE: 'Polysphere',
-        ZOOPOLIS: 'Zoopolis',
-        // BIKE: 'Bike Ride 3D',
-        // CLONE: 'My Clone Army',
-        // MOWTRIM: 'Mow and Trim',
-        // TWERK: 'Twerk Race 3D',
-        // MUDRACE: 'Mud Racing',
-        // CAFE: 'Cafe Dash',
-        // GANGWARS: 'Gang Wars',
-        // TILETRIO: 'Tile Trio',
-        // FLUFFCRUSADE: 'Fluff Crusade',
-        // STONEAGE: 'Stone Age',
-        // HIDEBALL: 'Hide Ball',
-        // PINOUT: 'Pin Out Master',
-        // AMONG: 'Among Water',
-    })
+    const app = [
+        { 
+            code: 'TRAIN', label: 'Train Miner', 
+            token: '82647f43-3f87-402d-88dd-09a90025313f', promo: 'c4480ac7-e178-4973-8061-9ed5b2e17954',
+            attempts: 10, delay: 2e4,
+        },
+        {
+            code: 'FCTRY', label: 'Factory World',
+            token: 'd02fc404-8985-4305-87d8-32bd4e66bb16', promo: 'd02fc404-8985-4305-87d8-32bd4e66bb16',
+            attempts: 10, delay: 4e4,
+        },
+        {
+            code: 'COUNT', label: 'Count Masters',
+            token: '4bdc17da-2601-449b-948e-f8c7bd376553', promo: '4bdc17da-2601-449b-948e-f8c7bd376553',
+            attempts: 20, delay: 4e4,
+        },
+        {
+            code: 'SNAKE', label: 'Snake Run',
+            token: 'c8e017e2-8817-4d02-bce6-b951e74bb18f', promo: 'c8e017e2-8817-4d02-bce6-b951e74bb18f',
+            attempts: 30, delay: 2e4,
+        },
+        {
+            code: 'MERGE', label: 'Merge Away',
+            token: '8d1cc2ad-e097-4b86-90ef-7a27e19fb833', promo: 'dc128d28-c45b-411c-98ff-ac7726fbaea4',
+            attempts: 10, delay: 2e4,
+        },
+        {
+            code: 'COOKST', label: 'Cooking Stories',
+            token: 'ed526e8c-e6c8-40fd-b72a-9e78ff6a2054', promo: 'ed526e8c-e6c8-40fd-b72a-9e78ff6a2054',
+            attempts: 10, delay: 4e4,
+        },
+        {
+            code: 'CUBE', label: 'Chain Cube 2048',
+            token: 'd1690a07-3780-4068-810f-9b5bbf2931b2', promo: '4170868-cef0-424f-8eb9-be0622e8e8e3',
+            attempts: 10, delay: 2e4,
+        },
+        {
+            code: 'INFCT', label: 'Infected Frontier',
+            token: 'eb518c4b-e448-4065-9d33-06f3039f0fcb', promo: 'eb518c4b-e448-4065-9d33-06f3039f0fcb',
+            attempts: 10, delay: 4e4,
+        },
+        {
+            code: 'BOUNCE', label: 'Bouncemasters',
+            token: 'bc72d3b9-8e91-4884-9c33-f72482f0db37', promo: 'bc72d3b9-8e91-4884-9c33-f72482f0db37',
+            attempts: 10, delay: 2e4,
+        },
+        {
+            code: 'POLYSPHERE', label: 'Polysphere',
+            token: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71', promo: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
+            attempts: 22, delay: 2e4,
+        },
+        {
+            code: 'ZOOPOLIS', label: 'Zoopolis',
+            token: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b', promo: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
+            attempts: 20, delay: 2e4,
+        },
+    ]
 
-    for (const [value, label] of Object.entries(apps)) {
+    for (const {code, label} of app) {
         const opt = document.createElement('option')
-        opt.value = value
+        opt.value = code
         opt.innerHTML = label
         cmbSelectedGame.appendChild(opt)
+        // console.log(entry)
     }
 
-    // APS Token
-    const tokens = Object.freeze({
-        BIKE: Object.freeze({ token: 'd28721be-fd2d-4b45-869e-9f253b554e50', promo: '43e35910-c168-4634-ad4f-52fd764a843f', attempts: 22, delay: 2e4, }),
-        CLONE: Object.freeze({ token: '74ee0b5b-775e-4bee-974f-63e7f4d5bacb', promo: 'fe693b26-b342-4159-8808-15e3ff7f8767', attempts: 11, delay: 7e4, }),
-        CUBE: Object.freeze({ token: 'd1690a07-3780-4068-810f-9b5bbf2931b2', promo: 'b4170868-cef0-424f-8eb9-be0622e8e8e3', attempts: 10, delay: 2e4, }),
-        TRAIN: Object.freeze({ token: '82647f43-3f87-402d-88dd-09a90025313f', promo: 'c4480ac7-e178-4973-8061-9ed5b2e17954', attempts: 10, delay: 2e4, }),
-        MERGE: Object.freeze({ token: '8d1cc2ad-e097-4b86-90ef-7a27e19fb833', promo: 'dc128d28-c45b-411c-98ff-ac7726fbaea4', attempts: 10, delay: 2e4, }),
-        TWERK: Object.freeze({ token: '61308365-9d16-4040-8bb0-2f4a4c69074c', promo: '61308365-9d16-4040-8bb0-2f4a4c69074c', attempts: 10, delay: 2e4, }),
-        POLYSPHERE: Object.freeze({ token: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71', promo: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71', attempts: 22, delay: 2e4 }),
-        MUDRACE: Object.freeze({ token: '8814a785-97fb-4177-9193-ca4180ff9da8', promo: '8814a785-97fb-4177-9193-ca4180ff9da8', attempts: 20, delay: 2e4 }),
-        MOWTRIM: Object.freeze({ token: 'ef319a80-949a-492e-8ee0-424fb5fc20a6', promo: 'ef319a80-949a-492e-8ee0-424fb5fc20a6', attempts: 20, delay: 2e4 }),
-        CAFE: Object.freeze({ token: 'bc0971b8-04df-4e72-8a3e-ec4dc663cd11', promo: 'bc0971b8-04df-4e72-8a3e-ec4dc663cd11', attempts: 20, delay: 2e4 }),
-        ZOOPOLIS: Object.freeze({ token: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b', promo: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b', attempts: 20, delay: 2e4 }),
-        GANGWARS: Object.freeze({ token: 'b6de60a0-e030-48bb-a551-548372493523', promo: 'c7821fa7-6632-482c-9635-2bd5798585f9', attempts: 30, delay: 4e4 }),
-        TILETRIO: Object.freeze({ token: 'e68b39d2-4880-4a31-b3aa-0393e7df10c7', promo: 'e68b39d2-4880-4a31-b3aa-0393e7df10c7', attempts: 20, delay: 2e4 }),
-        FLUFFCRUSADE: Object.freeze({ token: '112887b0-a8af-4eb2-ac63-d82df78283d9', promo: '112887b0-a8af-4eb2-ac63-d82df78283d9', attempts: 10, delay: 4e4 }),
-        STONEAGE: Object.freeze({ token: '04ebd6de-69b7-43d1-9c4b-04a6ca3305af', promo: '04ebd6de-69b7-43d1-9c4b-04a6ca3305af', attempts: 20, delay: 2e4 }),
-        BOUNCE: Object.freeze({ token: 'bc72d3b9-8e91-4884-9c33-f72482f0db37', promo: 'bc72d3b9-8e91-4884-9c33-f72482f0db37', attempts: 10, delay: 2e4 }),
-        HIDEBALL: Object.freeze({ token: '4bf4966c-4d22-439b-8ff2-dc5ebca1a600', promo: '4bf4966c-4d22-439b-8ff2-dc5ebca1a600', attempts: 20, delay: 3e4 }),
-        PINOUT: Object.freeze({ token: 'd2378baf-d617-417a-9d99-d685824335f0', promo: 'd2378baf-d617-417a-9d99-d685824335f0', attempts: 20, delay: 4e4 }),
-        COUNT: Object.freeze({ token: '4bdc17da-2601-449b-948e-f8c7bd376553', promo: '4bdc17da-2601-449b-948e-f8c7bd376553', attempts: 20, delay: 4e4 }),
-        INFCT: Object.freeze({ token: 'eb518c4b-e448-4065-9d33-06f3039f0fcb', promo: 'eb518c4b-e448-4065-9d33-06f3039f0fcb', attempts: 10, delay: 4e4 }),
-        AMONG: Object.freeze({ token: 'daab8f83-8ea2-4ad0-8dd5-d33363129640', promo: 'daab8f83-8ea2-4ad0-8dd5-d33363129640', attempts: 10, delay: 4e4 }),
-        FCTRY: Object.freeze({ token: 'd02fc404-8985-4305-87d8-32bd4e66bb16', promo: 'd02fc404-8985-4305-87d8-32bd4e66bb16', attempts: 10, delay: 4e4 }),
-        SNAKE: Object.freeze({ token: 'c8e017e2-8817-4d02-bce6-b951e74bb18f', promo: 'c8e017e2-8817-4d02-bce6-b951e74bb18f', attempts: 30, delay: 2e4 }),
-        COOKST: Object.freeze({ token: 'ed526e8c-e6c8-40fd-b72a-9e78ff6a2054', promo: 'ed526e8c-e6c8-40fd-b72a-9e78ff6a2054', attempts: 10, delay: 4e4 }),
-    })
     const generateId = (selectedGame) => {
         const fecha = Date.now();
         const random = localStorage.getItem(selectedGame) || Array.from({ length: 19 }, () => Math.floor(Math.random() * 10)).join("");
@@ -191,33 +196,37 @@ document.addEventListener('DOMContentLoaded', () => {
         setProgressBar(0)
 
         try {
+            // Obtener data del juego seleccionado 
+            const {token, promo, attempts, delay} = app.find(e => e['code'] == selectedGame)
+            console.log(token, promo, attempts, delay)
+            // return
             // Inicar sesion
             const litem = document.createElement('li')
             litem.appendChild(document.createTextNode('Iniciando Sesion'));
             ul.appendChild(litem)
-            const clientToken = await login(selectedGame, tokens[selectedGame]['token']);
+            const clientToken = await login(selectedGame, token);
             if (!clientToken) throw new Error(`[Login] Error -- No se ha iniciado sesion exitosamente`)
             litem.appendChild(document.createTextNode('...OK'));
             setProgressBar(15)
 
             // Registrar
             const lregister = document.createElement('li')
-            lregister.appendChild(document.createTextNode('Registrando'))
+            lregister.appendChild(document.createTextNode('Solicitando'))
             ul.appendChild(lregister)
             let r = null;
             let c = 0
-            for (; !r && c < tokens[selectedGame]['attempts']; c++) {
-                await sleep(tokens[selectedGame]['delay'] * delayRandom())
-                r = await register(clientToken, tokens[selectedGame]['promo']);
+            for (; !r && c < attempts; c++) {
+                await sleep(delay * delayRandom())
+                r = await register(clientToken, promo);
                 r ? lregister.appendChild(document.createTextNode('OK')) : lregister.appendChild(document.createTextNode('.'))
                 ul.appendChild(lregister)
-                setProgressBar(pBarPercentage + (70 / tokens[selectedGame]['attempts']))
+                setProgressBar(pBarPercentage + (70 / attempts))
             }
-            if (c > tokens[selectedGame]['attempts']) throw new Error('[Register] Error -- Se ha superado el limite de intentos.')
+            if (c > attempts) throw new Error('[Register] Error -- Se ha superado el limite de intentos.')
 
             // Obtener
             const lcreate = document.createElement('li')
-            const keyCode = await generate(clientToken, tokens[selectedGame]['promo']);
+            const keyCode = await generate(clientToken, promo);
             if (!keyCode) throw new Error('[Generate] Error -- Hubo un problema recuperando el codigo.')
             lcreate.appendChild(document.createTextNode('Codigo generado!'));
             ul.appendChild(lcreate)
@@ -250,12 +259,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const delayRandom = () => Math.random() / 3 + 1
 
-    const sleep = (time) => new Promise(r => setTimeout(r, time))
+    const sleep = (duration) => new Promise(r => setTimeout(r, duration))
 
-    const setProgressBar = (valor) => {
-        pBarPercentage = valor
-        pbar.style.width = `${valor}%`
-        pBarText.innerHTML = `${roundTo(valor, 2)}%`
+    const setProgressBar = (value) => {
+        pBarPercentage = value
+        pbar.style.width = `${value}%`
+        pBarText.innerHTML = `${roundTo(value, 2)}%`
     }
 
     const roundTo = (number, precision) => {
@@ -289,4 +298,5 @@ document.addEventListener('DOMContentLoaded', () => {
  * 1.2.16 Nuevos juegos: Factory World, Among Water, Infected Frontier
  * 1.2.17 Nuevos juegos: Cooking Stories, Snake Run
  * 1.2.17.1 Se retiran varios juegos.
+ * 1.2.18 Nuevo arreglo para hacer uso de una sola estructura de datos
  */
